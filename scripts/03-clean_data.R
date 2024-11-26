2f83b6f65ef7787dd00f68319a813a09e8a7bf4a
David Park <hyunje.park@mail.utoronto.ca>
2024-11-26 14:48:28 -0500

    renamed folders

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the children'sbook data
+# Author: Hyunje Park 
+# Date: November 26, 2024
+# Contact: hyunje.park@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
 library(tidyverse)
@@ -14,7 +13,7 @@ library(readr)
 library(arrow)
 
 #### Clean data ####
-data <- read.delim("data/01-raw_data/childrens-books.txt", header = TRUE, sep = "\t")
+data <- read.delim("data/raw_data/childrens-books.txt", header = TRUE, sep = "\t")
 print(data)
 
 # make a new column that determines if a book is high rated 
@@ -24,4 +23,4 @@ data$"rated_high" <- ifelse(data$rating > 4, 1, 0)
 data <- data %>% select(-c(isbn, original_publish_year))
 
 #### Save data in parquet file ####
-write_parquet(data, "data/02-analysis_data/analysis_data.parquet") 
+write_parquet(data, "data/analysis_data/analysis_data.parquet") 
