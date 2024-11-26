ce5199fc7133d3ecac94259756f4c0541665529a
David Park <hyunje.park@mail.utoronto.ca>
2024-11-26 15:46:53 -0500

    added all the visualizations in paper.qmd

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -4,7 +4,7 @@
 # Date: November 26, 2024
 # Contact: hyunje.park@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
+# Pre-requisites: None
 
 #### Workspace setup ####
 library(tidyverse)
@@ -20,7 +20,8 @@ print(data)
 data$"rated_high" <- ifelse(data$rating > 4, 1, 0)
 
 # drop unnecessary columns 
-data <- data %>% select(-c(isbn, original_publish_year))
+data <- data %>% select(-c(author, isbn, original_publish_year))
 
 #### Save data in parquet file ####
 write_parquet(data, "data/analysis_data/analysis_data.parquet") 
+
--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
 # Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Hyunje Park
+# Date: November 26, 2024
+# Contact: hyunje.park@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:
 
 
 #### Workspace setup ####
@@ -13,17 +12,16 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet("data/analysis_data/analysis_data.parquet")
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
+    rated_high ~ cover + rating_count + publish_year + pages,
     data = analysis_data,
-    family = gaussian(),
+    family = binomial(link = 'logit'),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
 
@@ -31,7 +29,7 @@ first_model <-
 #### Save model ####
 saveRDS(
   first_model,
-  file = "models/first_model.rds"
+  file = "models/model.rds"
 )
 
 
